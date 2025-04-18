7683df9d5dca71f5072b22e6cd6252eccf572e68
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2025-04-18 16:40:34 +0200

    Add Docker

---
+++ docker/entrypoint.dev.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Docker entrypoint for the development environment.
+
+# Stop on first return of a non-zero status
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#The-Set-Builtin
+set -e
+
+# Environment variables used to create Django super user.
+# https://docs.djangoproject.com/en/5.1/ref/django-admin/#createsuperuser
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=root@localhost
+export DJANGO_SUPERUSER_PASSWORD=123
+
+# Run database migration
+# This need to be first to avoid errors due missing tables
+python3 manage.py migrate \
+    --no-input \
+    --traceback
+
+# Remove all existing data
+python3 manage.py flush \
+    --no-input \
+    --traceback
+
+# Create Django super user
+# This need to be after load the fixtures
+# for the super user to have the correct password.
+createsuperuser.sh
+
+# Start Django development server
+python3 manage.py runserver \
+    0.0.0.0:8000
